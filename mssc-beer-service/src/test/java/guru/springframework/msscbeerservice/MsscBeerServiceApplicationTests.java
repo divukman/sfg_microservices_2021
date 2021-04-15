c8860b2d7fd3bf20170c5a3cc01aadb4e4e70439
dimitar <dimitar.vukman@bluedropmedical.com>
2021-04-15 18:33:40 +0200

    Fix tests.

--- mssc-beer-service/src/test/java/guru/springframework/msscbeerservice/MsscBeerServiceApplicationTests.java
+++ mssc-beer-service/src/test/java/guru/springframework/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -2,7 +2,9 @@ package guru.springframework.msscbeerservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("localh2")
 @SpringBootTest
 class MsscBeerServiceApplicationTests {
 
--- mssc-beer-service/src/test/java/guru/springframework/msscbeerservice/web/controller/BeerControllerTest.java
+++ mssc-beer-service/src/test/java/guru/springframework/msscbeerservice/web/controller/BeerControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
@@ -22,6 +23,7 @@ import java.util.UUID;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ActiveProfiles("localh2")
 @SpringBootTest
 @AutoConfigureMockMvc
 class BeerControllerTest {
