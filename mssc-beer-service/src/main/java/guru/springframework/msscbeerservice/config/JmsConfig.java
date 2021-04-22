9099ee66361be3bfb4baf60c8db65c88c5566034
dimitar <dimitar.vukman@bluedropmedical.com>
2021-04-22 11:55:22 +0200

    Local MQ config.

---
+++ mssc-beer-service/src/main/java/guru/springframework/msscbeerservice/config/JmsConfig.java
@@ -0,0 +1,28 @@
+package guru.springframework.msscbeerservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+
+@Configuration
+public class JmsConfig {
+    /**
+     * Bean -> message converter
+     * Using Jackson libraries to convert message to JSON
+     * ie Object -> To JSON String and vice versa, JSON String -> To Java Object
+     * @return converter
+     */
+    @Bean
+    public MessageConverter messageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+
+        return converter;
+    }
+
+}
